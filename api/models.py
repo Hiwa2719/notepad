b0a6cb9328c57576fe925ae4432c21fd715c01c6
hiwa2719 <hiahmadyan@gmail.com>
2022-04-25 13:24:28 +0430

    added Note model to admin

--- api/models.py
+++ api/models.py
@@ -4,8 +4,8 @@ from django.urls import reverse
 
 class Note(models.Model):
     text = models.TextField()
-    updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, help_text='last updated date')
+    created = models.DateTimeField(auto_now_add=True, help_text='created date')
 
     def __str__(self):
         return self.text[:55]
