f1a81e938143bf92da4cb5363500ee990bf96536
hiwa2719 <hiahmadyan@gmail.com>
2022-04-25 12:55:16 +0430

    added views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from .models import Note
+from .serializers import NoteSerializer
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view
+def get_notes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+@api_view
+def get_note(request, pk):
+    note = Note.objects.get(pk=pk)
+    serializer = NoteSerializer(note)
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def delete_note(request, pk):
+    note = Note.objects.get(pk=pk)
+    note.delete()
+    return Response(status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def create_note(request):
+    serializer = NoteSerializer(request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+def update_note(request, pk):
+    note = Note.objects.get(pk=pk)
+    serializer = NoteSerializer(note, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
