ef3256665e5aeb1b66c7237372ab53c6900c9560
hiwa2719 <hiahmadyan@gmail.com>
2022-04-25 12:41:37 +0430

    added api app an Note model

---
+++ api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Note(models.Model):
+    text = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text[:55]
+
+    def get_absolute_url(self):
+        return reverse('api:get_note', kwargs={'pk': self.pk})
