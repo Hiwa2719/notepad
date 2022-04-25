2dca6d8e1fdd1bd8ef969b2600a58a094ff35cba
hiwa2719 <hiahmadyan@gmail.com>
2022-04-25 12:43:38 +0430

    added NoteSerializer

---
+++ api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Note
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = 'id', 'text', 'updated', '__str__'
