65c03bfd71be530901bc6376147392f9a6a73ffa
hiwa2719 <hiahmadyan@gmail.com>
2022-04-27 18:52:06 +0430

    some changes to apis

--- api/views.py
+++ api/views.py
@@ -1,5 +1,4 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.response import Response
 
@@ -23,7 +22,10 @@ ORDER_BY = {
 
 
 class CheckModelMixin:
-    """This mixin checks whether this model name in url is matched with MODELS"""
+    """This mixin checks whether this model name in url is matched with MODELS
+    also it creates self.model self.order_by_string self.serializer_class
+    """
+
     def dispatch(self, request, *args, **kwargs):
         model = kwargs.get('model')
         if model not in MODELS:
@@ -35,6 +37,9 @@ class CheckModelMixin:
         self.serializer_class = SERIALIZERS[model]
         return super().dispatch(request, *args, **kwargs)
 
+    def get_queryset(self):
+        return self.model.objects.all()
+
 
 class ItemsListView(CheckModelMixin, ListAPIView):
     def get_queryset(self):
@@ -42,41 +47,15 @@ class ItemsListView(CheckModelMixin, ListAPIView):
 
 
 class ItemRetrieveView(CheckModelMixin, RetrieveAPIView):
-    def get_queryset(self):
-        return self.model.objects.all()
+    pass
 
 
 class ItemDeleteView(CheckModelMixin, DestroyAPIView):
     pass
 
 
-# @api_view(['DELETE'])
-# def delete_note(request, pk):
-#     note = Note.objects.get(pk=pk)
-#     note.delete()
-#     return Response(status=status.HTTP_200_OK)
-
-
-# @api_view(['POST'])
-# def create_note(request):
-#     serializer = NoteSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response(serializer.data)
-#     return Response(status=status.HTTP_400_BAD_REQUEST)
-
-
 class ItemCreateView(CheckModelMixin, CreateAPIView):
     pass
-
-# @api_view(['PUT'])
-# def update_note(request, pk):
-#     note = Note.objects.get(pk=pk)
-#     serializer = NoteSerializer(note, data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response(serializer.data)
-#     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class ItemUpdateView(CheckModelMixin, UpdateAPIView):
