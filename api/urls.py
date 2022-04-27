10ed7ca70c25a7ad2d1906d36552c772f4c4b53f
hiwa2719 <hiahmadyan@gmail.com>
2022-04-27 17:01:47 +0430

    some changes in urls in backend

--- api/urls.py
+++ api/urls.py
@@ -5,9 +5,11 @@ app_name = 'api'
 
 
 urlpatterns = [
-    path('', views.get_notes, name='get-note'),
-    path('<int:pk>/', views.get_note, name='get-note'),
-    path('delete/<int:pk>/', views.delete_note, name='delete-note'),
-    path('update/<int:pk>/', views.update_note, name='update-note'),
-    path('create/', views.create_note, name='create-note'),
+    path('notes/', include([
+        path('', views.get_notes, name='get-note'),
+        path('<int:pk>/', views.get_note, name='get-note'),
+        path('delete/<int:pk>/', views.delete_note, name='delete-note'),
+        path('update/<int:pk>/', views.update_note, name='update-note'),
+        path('create/', views.create_note, name='create-note'),
+    ])),
 ]
