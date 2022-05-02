ee33cce9c494d273b253a158ece0b1bae0248c8a
hiwa2719 <hiahmadyan@gmail.com>
2022-05-02 20:53:58 +0430

    adding delete functionality to passed task

--- frontend/src/components/ReminderModal.js
+++ frontend/src/components/ReminderModal.js
@@ -1,12 +1,25 @@
 import React from "react";
+import axios from "axios";
 
 
 const ReminderModal = ({openModal, setOpenModal, note}) => {
-
     const closeHandler = () => {
         setOpenModal(false)
     }
 
+    const deleteTask = () =>{
+        axios.delete(`/api/tasks/delete/${note.id}/`)
+            .then(response=>{
+                setOpenModal(false)
+                console.log('success')
+                window.location.reload()
+            })
+            .catch(error=>{
+                console.log('error')
+                console.log(error)
+                alert('Something went wrong we couldn\'t do your request')
+            })
+    }
     if (!openModal) return
     return (
         <div className="overlay" onClick={closeHandler}>
@@ -15,7 +28,7 @@ const ReminderModal = ({openModal, setOpenModal, note}) => {
                 <div className="modal-inner">
                     <h3>You sat following reminder for this time: {note.formatted_updated}</h3>
                     <p>{note.text}</p>
-                    <button className="btn btn-warning">Delete</button>
+                    <button className="btn btn-warning" onClick={deleteTask}>Delete</button>
                 </div>
             </div>
         </div>
