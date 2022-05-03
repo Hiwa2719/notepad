751725c23bac7f743f274bb6fe498fbc274969bb
hiwa2719 <hiahmadyan@gmail.com>
2022-05-03 12:32:46 +0430

    changing notes&tasks list time to local time

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Link} from 'react-router-dom'
+import moment from "moment";
 
 
 class ListItem extends React.Component {
@@ -7,18 +8,26 @@ class ListItem extends React.Component {
         super();
         this.itemRef = React.createRef()
         this.state = {
-            openModal: false
+            time: null
         }
+    }
+
+    componentDidMount() {
+        let {note} = this.props
+        this.setState({
+            time: moment(note.reminder_time).format('MM/DD/YYYY HH:mm:SS')
+        })
     }
 
     render() {
         const {note} = this.props
+        const {time} = this.state
         return (
             <div>
                 <Link to={note.get_absolute_url} ref={this.itemRef}
                       className="d-block text-decoration-none text-light border-bottom border-dark border-1 fs-6 p-3 list-item">
                     <h4>{note.__str__}</h4>
-                    <small>{note.formated_updated}</small>
+                    <small>{time}</small>
                 </Link>
             </div>
         )
